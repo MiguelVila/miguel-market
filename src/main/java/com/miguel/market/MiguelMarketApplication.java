5d8369fb63440c0be5506e6acbcde1227aa86769
Miguel Vila u <miguelangelvilahuallpa@MacBook-Air-de-Miguel.local>
2022-11-26 02:55:36 -0500

    hello world

---
+++ src/main/java/com/miguel/market/MiguelMarketApplication.java
@@ -0,0 +1,13 @@
+package com.miguel.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiguelMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiguelMarketApplication.class, args);
+	}
+
+}
